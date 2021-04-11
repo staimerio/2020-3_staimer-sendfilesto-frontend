8aac915791846460d5c33010756f96466042e992
Braian Staimer <braian.florian@>
2021-04-11 10:30:06 -0600

    + adscash

--- src/app/main/2020-6-SENDFILES/2020-5-CLOUD-STORAGE/Ads/FooterFile.js
+++ src/app/main/2020-6-SENDFILES/2020-5-CLOUD-STORAGE/Ads/FooterFile.js
@@ -5,15 +5,21 @@ import reducer from '../store/reducers';
 
 function FooterFile(props) {
 	useEffect(() => {
+		// Adskeeper
 		const script = document.createElement('script');
-
 		script.src = 'https://jsc.adskeeper.com/s/e/sendfiles.to.1070066.js';
 		script.async = true;
-
 		document.body.appendChild(script);
-
+		// Adcash
+		const scriptPop = document.createElement('script');
+		scriptPop.src = 'https://asacdn.com/script/atg.js';
+		scriptPop.setAttribute('data-adel', 'atag');
+		scriptPop.setAttribute('czid', 'r4u6f25l');
+		scriptPop.async = true;
+		document.body.appendChild(scriptPop);
 		return () => {
 			document.body.removeChild(script);
+			document.body.removeChild(scriptPop);
 		};
 	}, []);
 
